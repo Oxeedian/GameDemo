6fd23bcc4fa20898b23d8659bd706b657d4e276a
Oscar Ruselius <karl-ekarnan@outlook.com>
2025-05-07 10:46:35 +0200

    Empty scripts and Game Demo Log

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    enum GameState
+    {
+        MainMenu,
+        Base,
+        Game
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
